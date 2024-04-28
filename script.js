bf2f24fcb48ad125afb5a4278d008a53101a2686
Antriksh rawat <156641834+unihack2@users.noreply.github.com>
2024-04-28 22:25:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let colours = {
           6: "red"
 };
 let time = 1100;
-function dance() {
+function inifinity() {
           let p1 = Math.floor(Math.random() * 18);
           let p2 = Math.floor(Math.random() * 18);
           let num = Math.floor(Math.random() * 7);
@@ -31,6 +31,8 @@ function dance() {
 };
 let p = document.querySelector("p");
 let h = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+let slt = document.querySelector("select");
 let score = 0;
 let highscore = 0;
 let check = () => {
@@ -40,23 +42,35 @@ let check = () => {
                     time = time-8;
                      p.innerText = "Score = " + score;
                      bdy.removeChild(cl);
-                     dance();
+                     inifinity();
           });
 };
 let check2 = () => {
           if (st.disabled == true) {
-                    dance();
+                    inifinity();
           } else {
                     return 0;
           }
 }
+slt.addEventListener("click",()=>{
+          if(slt.value=="two") {
+                    h3.style.color = "#82667F";
+          }else {
+                    h3.style.color = "black"; 
+          }
+})
 let st = document.getElementById("st");
 let rs = document.getElementById("rs");
 st.addEventListener("click", () => {
+          if(slt.value=="one") {
+             h3.innerText = "59s";
+          timer();       
+          }
+          slt.disabled = true;
           st.disabled = true;
           st.style.transform = "scale(0.8)";
-          dance();
-          rs.addEventListener("click", () => {
+          inifinity();
+          rs.addEventListener("click", reset=() => {
                     st.style.transform = "scale(1)";
                     rs.style.transform = "scale(0.9)";
                     setInterval(()=>{
@@ -69,6 +83,19 @@ st.addEventListener("click", () => {
                     score = 0;
                     time = 1000;
                     st.disabled = false;
+                    slt.disabled = false;
+                    h3.innerText = "1m"
                     p.innerText = "Score = 0";
           })
 });
+let timer=()=>{
+          setTimeout(()=>{
+                    let time = parseFloat(h3.innerText);
+                    if(time==0||slt.disabled==false) {
+                              reset();
+                              return 0;
+                    }
+                    h3.innerText = (time-1)+"s";  
+                    timer();
+          },1000);
+}
